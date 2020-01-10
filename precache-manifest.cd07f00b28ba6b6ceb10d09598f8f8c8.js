f2a315698e2505e05bc82b63dcabf4994a0c1d0f
ulyavrubel <uvrubel@gmail.com>
2020-01-10 15:07:32 +0800

    Updates

---
+++ precache-manifest.cd07f00b28ba6b6ceb10d09598f8f8c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef172538685927ae0002c2da8ffb8212",
+    "url": "/circleApp/index.html"
+  },
+  {
+    "revision": "f74b97a04ab9982e122e",
+    "url": "/circleApp/static/js/2.6fc2f1d7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/circleApp/static/js/2.6fc2f1d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6003b131fb04728dc64b",
+    "url": "/circleApp/static/js/main.e7f952cb.chunk.js"
+  },
+  {
+    "revision": "7f96a5bff00ba6c088b8",
+    "url": "/circleApp/static/js/runtime-main.fbef51b6.js"
+  }
+]);
